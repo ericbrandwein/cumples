de205a31f3973ca6954f4160893dea72b3ba1301
candecp <cande.cp@gmail.com>
2015-03-12 14:43:43 -0300

    Hecho el service del envio mail!

--- test/unit/cumples/EditEmpleadoControllerSpec.groovy
+++ test/unit/cumples/EditEmpleadoControllerSpec.groovy
@@ -6,7 +6,7 @@ import spock.lang.Specification
 /**
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
-@TestFor(EditEmpleadoController)
+@TestFor(EditController)
 class EditEmpleadoControllerSpec extends Specification {
 
     def setup() {
