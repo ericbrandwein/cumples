daa46c8838f855da6818e56811e8f6ae9e0a0ce0
Diego Sebastian Garcia <diego.garcia@mercadolibre.com>
2015-03-13 11:32:31 -0300

    Agregado plugin de security en BuildConfig.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -64,7 +64,8 @@ grails.project.dependency.resolution = {
         compile ":asset-pipeline:1.9.9"
 		compile ":mail:1.0.7" //agregado para plugin mail
 		
-
+        compile ':spring-security-core:2.0-RC4'
+        
         // plugins needed at runtime but not for compilation
         runtime ":hibernate4:4.3.6.1" // or ":hibernate:3.6.10.18"
         runtime ":database-migration:1.4.0"
