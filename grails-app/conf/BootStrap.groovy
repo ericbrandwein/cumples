a98811ddd8c3ac754488eb5f9cff76b606add73f
Eric Brandwein <eric.brandwein@mercadolibre.com>
2015-03-16 11:13:45 -0300

    #3 Agregadas instancias de empresas al Bootstrap.groovy.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -8,9 +8,16 @@ class BootStrap {
     	def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true) 
     	def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
 		def testUser = new User(username: 'me', password: 'password')
+		def empresa1= new Empresa(nombre:"meli").addToAdministradores(testUser)
 
+		def testUser2 = new User(username: 'me2', password: 'password')
+		def empresa2= new Empresa(nombre:"amazon").addToAdministradores(testUser2)
+		testUser2.save(flush: true)
 		testUser.save(flush: true)
+		empresa1.save(flush:true)
+		empresa2.save(flush:true)
 		UserRole.create (testUser, adminRole, true)
+		UserRole.create (testUser2, adminRole, true)
     }
     
     def destroy = {
