e8f6fe224533cb7e44cb142b19e48809be47b9a0
Diego Sebastian Garcia <diego.garcia@mercadolibre.com>
2015-03-16 14:55:36 -0300

    #3 Modificados los permisos de empleadosController. Modificado el Bootstrap.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -11,16 +11,26 @@ class BootStrap {
 
 		def testUser = new User(username: 'me', password: 'password')
 		def testUser2 = new User(username: 'me2', password: 'password')
+
+		def empresa1= new Empresa(nombre:"meli").addToAdministradores(testUser)
+		def empresa2= new Empresa(nombre:"amazon").addToAdministradores(testUser2)
+		
+		empresa1.save(flush: true)
+		empresa2.save(flush: true)
 		testUser.save(flush: true)
 		testUser2.save(flush: true)
+
+		def roleAdmin = Role.findByAuthority('ROLE_ADMIN')
+		testUser.validate()
+		println testUser.errors
+		UserRole.create testUser, roleAdmin, true
+		//def roleAdmin = Role.findByName('ROLE_ADMIN')
+		UserRole.create testUser2, roleAdmin, true
 
 		UserRole.create (testUser, adminRole, true)
 		UserRole.create (testUser2, adminRole, true)
 
-		def empresa1= new Empresa(nombre:"meli").addToAdministradores(testUser)
-		empresa1.save(flush: true)
-		def empresa2= new Empresa(nombre:"amazon").addToAdministradores(testUser2)
-		empresa2.save(flush: true)
+		
 
 		def fecha=new Date()
 		fecha.setDate(16)
@@ -32,11 +42,9 @@ class BootStrap {
 		regalo1.save(flush: true)
 		empleado1.addToRegalos(regalo1)
 		empleado1.save(flush: true, failOnError: true)
-   		empresa1.addToEmpleados(empleado1)
-   		empresa1.save(flush: true)
    		empresa2.addToEmpleados(empleado1)
    		empresa2.save(flush: true)
-		
+   		//empresa2.addToAdministradores(testUser)
 		
     }
     
--- grails-app/controllers/cumples/EmpleadosController.groovy
+++ grails-app/controllers/cumples/EmpleadosController.groovy
@@ -11,16 +11,17 @@ class EmpleadosController {
 	
 	def agregarC(){
 		if(Empleado.findByDni(params.dni)==null){
-			def nombre=params.nombre;
-			def apellido=params.apellido;
-			def dni=params.dni;
-			def dia=params.dia;
-			def mes=params.mes;
-			def anio=params.anio;
+			def usuario = getCurrentUser()
+			def nombre=params.nombre
+			def apellido=params.apellido
+			def dni=params.dni
+			def dia=params.dia
+			def mes=params.mes
+			def anio=params.anio
 
-			def role=params.role;
 			Empleado e1= new Empleado(nombre: nombre, apellido: apellido, dni: dni, fechaNacimiento: Date.parse("dd-MM-yyyy", dia+"-"+mes+"-"+anio));
 			e1.save(failOnError:true);
+			usuario.empresa.addToEmpleados(e1)
 			flash.message="success";
 			redirect(action:'index');
 		}
